f77897faa1a4e0aab9e3e7558d8d36e533e9ee92
zhangchengfeng01 <zhangchengfeng01@baidu.com>
2021-01-07 10:41:54 +0800

    demo easypoi 实现简单导出初版代码

---
+++ src/main/java/com/example/demo/easypoi/PO/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.easypoi.PO;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import cn.afterturn.easypoi.excel.annotation.ExcelIgnore;
+import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@ExcelTarget("users") //必须要实现序列化
+public class User implements Serializable {
+    @Excel(name = "编号")
+    private String id;
+    @Excel(name = "姓名")
+    private String name;
+    @Excel(name = "年龄",orderNum="1",suffix = "岁")
+    private Integer age;
+    @Excel(name = "生日",width = 35.0,format = "yyyy-MM-dd HH:mm:ss")
+    private Date bir;
+    @ExcelIgnore
+    private List<String> hobby;
+    @Excel(name = "爱好",width = 20.0,orderNum = "5")
+    private String hobbys;
+    @Excel(name = "头像",width = 20,type = 2)
+    private String photo;
+
+    public String getHobbys() {
+        StringBuilder sb = new StringBuilder();
+        hobby.forEach(e->{
+            sb.append(e).append("、");
+        });
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/example/demo/easypoi/EasyPoiTest.java
@@ -0,0 +1,59 @@
+package com.example.demo.easypoi;
+
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import com.example.demo.easypoi.PO.User;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class EasyPoiTest {
+    //查询所有记录 模拟数据库
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            User user = new User();
+            user.setId(String.valueOf(i));
+            user.setAge(10+i);
+            user.setName("小黄"+i);
+            user.setBir(new Date());
+            user.setHobby(Arrays.asList("烫头，抽烟"));
+            users.add(user);
+        }
+        return users;
+    }
+    //导出Excel
+    @Test
+    public void testExport() throws IOException {
+        //获取数据
+        List<User> users = getUsers();
+        //导出Excel
+        //参数1、exportParams 导出配置对象， 参数2导出的类型，参数3 导出数据集合
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("用户信息列表", "用户信息"), User.class, users);
+        //将excel写入指定位置
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream("/Users/zhangchengfeng01/Downloads/cc.xls");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        outputStream.close();
+        workbook.close();
+
+
+    }
+
+}
