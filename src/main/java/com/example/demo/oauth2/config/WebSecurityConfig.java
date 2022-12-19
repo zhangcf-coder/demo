1da5f08054819b1b16afd47b582af5fc5f0c51a4
zhangchengfeng <zhangchengfeng@idongwo.com>
2022-12-19 09:42:46 +0800

    1.oauth2代码

---
+++ src/main/java/com/example/demo/oauth2/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().permitAll()
+                .and().authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().permitAll()
+                .and().csrf().disable();
+    }
+}
